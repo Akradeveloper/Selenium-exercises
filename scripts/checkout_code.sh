58ee809b82487d474f6b158f85f2050ea3baee94
Akrapovic <akradeveloper@gmail.com>
2024-11-16 22:09:37 +0100

    rollback

--- scripts/checkout_code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Checkout the code from the specified repository
-
-echo "Checking out the code..."
-git clone https://github.com/Akradeveloper/Selenium-exercises.git
-cd Selenium-exercises || exit 1
-git checkout "${GITHUB_REF}"
