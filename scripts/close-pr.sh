f80d0e64808433d624951ee2054951ae89d5cf89
Akrapovic <akradeveloper@gmail.com>
2024-11-18 19:01:11 +0100

    rollback

--- scripts/close-pr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PR_NUMBER=$1
-
-# Cerrar el PR si hubo un error
-curl -X PATCH -H "Authorization: token ${{ secrets.TOKEN_GITHUB }}" \
-  -d "{\"state\": \"closed\"}" \
-  "https://api.github.com/repos/${{ github.repository }}/pulls/$PR_NUMBER"
