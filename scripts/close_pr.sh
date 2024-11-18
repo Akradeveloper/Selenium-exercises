3658fffcc97725cd4ca8d9772f63ca0bb7629722
Akrapovic <akradeveloper@gmail.com>
2024-11-18 20:44:52 +0100

    new

--- scripts/close_pr.sh
+++ scripts/close_pr.sh
@@ -2,14 +2,16 @@
 
 # Leer los errores del archivo generado en la validación
 errores=$(cat validation_errors.txt)
-
+TOKEN_GITHUB=$1
+REPOSITORY=$2
+PR=$3
 # Si hay errores, agregar un comentario en el PR
 if [[ -n "$errores" ]]; then
   echo "Se encontraron errores, enviando comentario en el PR..."
 
   curl -X POST -H "Authorization: token $TOKEN_GITHUB" \
        -d "{\"body\": \"${errores}\"}" \
-       "https://api.github.com/repos/${{ github.repository }}/issues/${{ github.event.pull_request.number }}/comments"
+       "https://api.github.com/repos/$REPOSITORY/issues/$PR/comments"
 else
   echo "No se encontraron errores."
 fi
@@ -17,4 +19,4 @@ fi
 # Opcional: Si deseas cerrar el PR si hay errores graves, lo puedes hacer aquí
 curl -X PATCH -H "Authorization: token $TOKEN_GITHUB" \
      -d '{"state": "closed"}' \
-     "https://api.github.com/repos/${{ github.repository }}/pulls/${{ github.event.pull_request.number }}"
+     "https://api.github.com/repos/$REPOSITORY/pulls/$PR"
