103659c9f0495e50ea6eacaaf084590035987954
Akrapovic <akradeveloper@gmail.com>
2024-11-18 16:00:41 +0100

    new flow

--- scripts/close-pr.sh
+++ scripts/close-pr.sh
@@ -3,6 +3,6 @@
 PR_NUMBER=$1
 
 # Cerrar el PR si hubo un error
-curl -X PATCH -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+curl -X PATCH -H "Authorization: token ${{ secrets.TOKEN_GITHUB }}" \
   -d "{\"state\": \"closed\"}" \
   "https://api.github.com/repos/${{ github.repository }}/pulls/$PR_NUMBER"
--- scripts/post-results.sh
+++ scripts/post-results.sh
@@ -5,11 +5,11 @@ ERROR_MESSAGES=$2
 PR_NUMBER=$3
 
 if [ "$ERROR_FLAG" -eq 1 ]; then
-  curl -X POST -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+  curl -X POST -H "Authorization: token ${{ secrets.TOKEN_GITHUB }}" \
     -d "{\"body\": \"❌ Errores encontrados en la validación del PR:\n$ERROR_MESSAGES\"}" \
     "https://api.github.com/repos/${{ github.repository }}/issues/$PR_NUMBER/comments"
 else
-  curl -X POST -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+  curl -X POST -H "Authorization: token ${{ secrets.TOKEN_GITHUB }}" \
     -d "{\"body\": \"✅ Validación completada sin errores.\"}" \
     "https://api.github.com/repos/${{ github.repository }}/issues/$PR_NUMBER/comments"
 fi
